b1c42b81aa0dec0169243b09b6ee3051c338ff7a
Nei1eveN <neilpotot@gmail.com>
2020-05-21 01:09:08 +0800

    Search Function 5-21-2020
    - add function for finding object by collectionId
    - add functions to repository
    - adjust filtering for visited tracks
    - add function for clicked items
    - add trackId generation for audiobooks/items that does not have trackId
    - change background tint according to theme colors
    - add direction of navigation from search to track details

--- app/src/main/java/com/appetiser/appetiserapp1/data/dao/TrackDao.kt
+++ app/src/main/java/com/appetiser/appetiserapp1/data/dao/TrackDao.kt
@@ -4,8 +4,13 @@ import com.appetiser.appetiserapp1.core.dao.TrackBaseDao
 import com.appetiser.appetiserapp1.data.model.Track
 import com.appetiser.appetiserapp1.extensions.find
 import io.realm.Realm
+import io.realm.kotlin.where
 
 class TrackDao(private val realm: Realm) : TrackBaseDao<Track>(realm, Track::class.java) {
 
     override fun findById(id: Int): Track? = realm.find(id)
+
+    fun findByCollectionId(collectionId: Int): Track? {
+        return realm.where<Track>().equalTo("collectionId", collectionId).findFirst()
+    }
 }
--- app/src/main/java/com/appetiser/appetiserapp1/data/repository/TrackRepository.kt
+++ app/src/main/java/com/appetiser/appetiserapp1/data/repository/TrackRepository.kt
@@ -1,10 +1,9 @@
 package com.appetiser.appetiserapp1.data.repository
 
-import com.airbnb.mvrx.Async
-import com.appetiser.appetiserapp1.core.awaitAsync
 import com.appetiser.appetiserapp1.core.dao.RealmBaseRepository
 import com.appetiser.appetiserapp1.data.dao.TrackDao
 import com.appetiser.appetiserapp1.data.model.SearchResult
+import com.appetiser.appetiserapp1.data.model.Track
 import com.appetiser.appetiserapp1.data.network.SearchApiClient
 import com.appetiser.appetiserapp1.extensions.asCallbackFlow
 import io.realm.Realm
@@ -17,6 +16,12 @@ class TrackRepository(
 ) : RealmBaseRepository(realm) {
 
     fun getTracks() = trackDao.findAllAsync().asCallbackFlow()
+
+    fun findById(trackId: Int) = trackDao.findById(trackId)
+
+    fun findByCollectionId(collectionId: Int) = trackDao.findByCollectionId(collectionId)
+
+    fun addItem(item: Track) = trackDao.add(item)
 
     fun searchTerm(term: String): Call<SearchResult> =
         searchApiClient.getService().searchTerm(term)
